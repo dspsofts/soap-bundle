0d4217b0297efdfaa6702f81d4b3ba3e8428f98b
Pierre Feyssaguet <pierre.feyssaguet@grosbill.com>
2015-04-16 10:01:35 +0200

    Initial import

---
+++ src/DspSofts/SoapBundle/DspSoftsSoapBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bundle de gestion de webservices SOAP.
+ *
+ * @author Pierre Feyssaguet <pfeyssaguet@gmail.com>
+ * @since 16/04/15 10:00
+ */
+
+namespace DspSofts\SoapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DspSoftsSoapBundle extends Bundle
+{
+
+}
